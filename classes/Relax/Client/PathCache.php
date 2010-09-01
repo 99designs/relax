aa2e1f7d16361e97014e68a8e65dec7828cb96d0
Dennis Hotson <dennis@99designs.com>
2010-09-02 08:59:45 +1000

    Implemented a "path cache" that reads extra preloaded data in response.

---
+++ classes/Relax/Client/PathCache.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * A wrapper for a connection that can populate a cache based on extra
+ * data returned in the response
+ */
+class Relax_Client_PathCache implements Relax_Client_Connection
+{
+	private $_cache = array();
+
+	public function __construct($connection)
+	{
+		$this->_connection = $connection;
+	}
+
+	/* (non-phpdoc)
+	 * @see Relax_Client_Connection::put()
+	 */
+	function get($path)
+	{
+		if (isset($this->_cache[$path]))
+			return $this->_cache[$path];
+
+		$obj = $this->_connection->get($path);
+
+		$this->_cache($path, $obj);
+
+		return $obj;
+	}
+
+	private function _cache($base, $obj)
+	{
+		$this->_cache[$base] = $obj;
+		foreach ($obj as $k => $v)
+		{
+			if (is_array($v) || is_object($v))
+				$this->_cache("$base/$k", $v);
+		}
+	}
+
+	// ---- All other operations invalidate the cache...
+
+	/* (non-phpdoc)
+	 * @see Relax_Client_Connection::put()
+	 */
+	function delete($path)
+	{
+		$this->_cache = array();
+		return $this->_connection->delete($path);
+	}
+	/* (non-phpdoc)
+	 * @see Relax_Client_Connection::put()
+	 */
+	function put($path, $body)
+	{
+		$this->_cache = array();
+		return $this->_connection->put($path, $body);
+	}
+
+	/* (non-phpdoc)
+	 * @see Relax_Client_Connection::put()
+	 */
+	function post($path, $body)
+	{
+		$this->_cache = array();
+		return $this->_connection->post($path, $body);
+	}
+}
