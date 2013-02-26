844a2468537526ac9db43960ceef5749df674d1b
richo <richo@psych0tik.net>
2013-02-26 17:29:21 +1100

    Test passphrases are correctly checked

---
+++ tests/Relax/Openssl/PrivateKeyTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @author Richo Healey <richo@99designs.com>
+ */
+class Relax_Openssl_PrivateKeyTest extends UnitTestCase
+{
+	public function setUp()
+	{
+		$this->privateKey = tmpfile();
+		$this->passPhrase = "llamas love long passphrases";
+
+		$this->pkey = openssl_pkey_new();
+		openssl_pkey_export($this->pkey, $pkey, $this->passPhrase);
+		fwrite($this->privateKey, $pkey);
+		fflush($this->privateKey);
+		$pkey_metadata = stream_get_meta_data($this->privateKey);
+		$this->privateKeyPath = $pkey_metadata["uri"];
+	}
+
+	public function testValidPassphrase()
+	{
+		$pkey = new Relax_Openssl_PrivateKey(
+			$this->privateKeyPath, false,
+			$this->passPhrase
+		);
+
+		$this->assertTrue($pkey->isValid());
+	}
+
+	public function testInvaludPassphrase()
+	{
+		$pkey = new Relax_Openssl_PrivateKey(
+			$this->privateKeyPath, false,
+			"llamas hate invalid passphrases"
+		);
+
+		$this->assertFalse($pkey->isValid());
+	}
+}
